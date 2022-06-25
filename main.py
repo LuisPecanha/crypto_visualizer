6149abc11d063e242418d75f9d14a5719b8957a7
LuisPecanha <luisgustavopxv@hotmail.com>
2022-06-25 20:47:08 -0300

    Implement human date to epoch function

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-import requests
 import csv
 import json
-import ciso8601
+from datetime import datetime
+import time
+import requests
+
+
+def date_to_unix_miliseconds(human_date_string):
+
+    # TODO - Add verification if it is a date in the specified format.
+    # TODO - Add verification to see if it is not a future date. (do this with a class for date manipulation ?)
+
+    dt_date = datetime.strptime(human_date_string, "%Y-%m-%d").date()
+
+    result_epoch_string = str(
+        int(time.mktime(dt_date.timetuple()) * 1000)
+    )  # int to remove float decimal
+
+    print(result_epoch_string)
+
 
 url = "http://api.coincap.io/v2/assets/{}/history?interval={}&start={}&end={}"
+
+
+date_to_unix_miliseconds("2011-12-01")
